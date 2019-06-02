eb5468d7dcd0cf9af02e5136a6c2851de7119792
Ben Diegel <b.diegelg@gmail.com>
2019-06-01 20:33:25 -0400

    added lots of things

--- textGame.py
+++ textGame.py
@@ -3,21 +3,37 @@ import os
 
 #This is the class information of the main, player character.
 class MainCharacter():
-    def __init__(self, name, age, xp=0):
+    def __init__(self, name, age):
         self.name = name
         self. age = age
-        self.xp = xp
-        self.hp = 100
+        self.xp = 0
+        self.hp = 10
         self.level = 1
-        self.attack = random.randint(2,8)
+        self.attack = random.randint(2,3)
+        self.deathCount = 0
+        self.killCount = 0
 
 class EasyBadGuy():
     def __init__(self, xp = 5):
         self.name = "Goblin"
         self.hp = random.randint(3,9)
         self.xp = xp
-        self.attack = random.randint(1,2)
+        self.attack = random.randint(1,3)
         self.luck = 1
+class MedBadGuy():
+    def __init__(self, xp = 12):
+        self.name = "Troll"
+        self.hp = random.randint(10,22)
+        self.xp = xp
+        self.attack = random.randint(7,16)
+        self.luck = 2
+class HardBadGuy():
+    def __init__(self, xp = 20):
+        self.name = "Ogre"
+        self.hp = random.randint(23,35)
+        self.xp = xp
+        self.attack = random.randint(20,40)
+        self.luck = 3
 
 #The main funtion. It starts the program then sets the players character
 #whether its loaded in or created.
@@ -52,23 +68,48 @@ def startScreen():
 
 #this is the main loop. generates bad guys, has a shop, picks abilites
 def mainLoop(p1):
-    os.system("clear")
-    #populates an easy bad guy for the player
-    easyBadGuy = EasyBadGuy() 
-    print("Welcome " + p1.name + " you are looking mighty tough!\n" +
-          "Fight this " + easyBadGuy.name + ", you " + str(p1.age) + " year old!!\n")
-    adv()
-    fightScreen(p1, easyBadGuy)
+    userChoice = 0
+    #the loop of the program. everything comes back to here unless they press 4 to exit
+    while (userChoice != '4'):
+        os.system("clear")
+
+        #checks to see if the player has enough health 
+        if (p1.hp <= 0):
+            death(p1)
+        #sees whether the player wants to fight, shop, train, or exit
+        userChoice = townInfo()
+
+        if (userChoice == '1'): #fight choice            
+            fightScreen(p1)
+        elif (userChoice == '2'): #shop choice
+            #the shop is where the player can buy stat upgrade trough weapons and armor
+            print ("you are in the shop!")
+            adv()
+        elif (userChoice == '3'): #learn spells choice
+            #the libray is where the player learns new attacks and sees their stats
+            print("you are in the library")
+            adv()
+    exit
 
 #The screen to call up when you encounter a monster
-def fightScreen(p1, enemy):
+def fightScreen(p1):
     os.system("clear")
+
+    #populates and enemy for the fight.
+    enemy = enemyEncounter(p1)
+    os.system("clear")
+
+    print(p1.name + " stumbled apon a ferocious " + enemy.name + "!!")
+    adv()
+
     #checks to see if the current enemy is dead or not
     while (enemy.hp > 0):
         os.system("clear")
+
         printInfo(enemy)
         print()
-        print("Your HP: " + str(p1.hp) + "\n")
+        printInfo(p1)
+        print()
         #prints the enemy infor ^^ then asks the player what to do
         playerOption = input("- What would you like to do? -\n"+
                          "1. Fight\n"+
@@ -83,18 +124,53 @@ def fightScreen(p1, enemy):
                 #tests the enemies luck against 1-3 (1 being lowest, 3 being never misses)
                 if (enemy.luck >= random.randint(1,3)):
                     attack(enemy, p1)
-                    adv()
+
+                    #checks to see if the player is dead or not
+                    if (p1.hp <= 0):
+                        death(p1)
+                        return
+                    else:
+                        adv()
                 else:
                     print("But misses!")
                     adv()
-        else:
-            print("")
-    
+        elif (playerOption == '2'):
+            os.system("clear")
+            #WIP WIP WIP
+            #if you run away you get no exp. will be a random chance though 
+            print("you coward!")
+            enemy.hp = 0
+            adv()
+            return
+        
     adv()
     #prints out the player xp amount if the enemy is beaten
     os.system("clear")
     gainXp(p1, enemy)
-    
+    adv()
+
+#when the player dies during their adventure they come here to print out stats and start over
+def death(p1):
+    os.system("clear")
+
+    print("you ded")
+    print("You earned " + str(p1.xp) + " exp!")
+    p1.deathCount = p1.deathCount + 1
+    print("You have died " + str(p1.deathCount) + " times on your journey so far!")
+    p1.hp = 10
+    adv()
+
+#choses what enemy the player faces based on how many kills. 
+#this needs more complexity 
+def enemyEncounter(p1):
+    if (p1.killCount <= 5):
+        enemy = EasyBadGuy()
+    elif (p1.killCount <= 20):
+        enemy = MedBadGuy()
+    elif (p1.killCount <= 40 or p1.killCount > 40):
+        enemy = HardBadGuy()
+    return enemy
+
 
 #Function for when someone atacks something
 def attack(attacker, attackie):
@@ -104,12 +180,28 @@ def attack(attacker, attackie):
     print(attackie.name + "'s hp is now " + str(attackie.hp))
     print()
 
+#anytime the player gains exp through battle it comes here
 def gainXp(p1, enemy):
     print("You BEAT the " + enemy.name)
     print("You gained " + str(enemy.xp) + " experience!")
     p1.xp = p1.xp + enemy.xp
+    p1.killCount = p1.killCount + 1
     print("For a player total of " + str(p1.xp))
 
+#prints out the town info and grabs the players choice
+def townInfo():
+    os.system("clear")
+
+    print("Welcome to Stabby Village!")
+    print("What would you like to do?\n")
+    print("1. Fight monsters and gain rewards!")
+    print("2. Spend that sweet, sweet gold.")
+    print("3. Train in the magical arts.")
+    print("4. Exit the town.")
+
+    userChoice = input(">>")
+
+    return userChoice
 
 #This is where the player is sent to to create their new character. 
 def characterCreation():
